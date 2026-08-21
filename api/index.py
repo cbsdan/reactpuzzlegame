37998a6ff1162a060f5134200c1f366a4e8a9d1b
Justine Juliana Balla <justinejuliana.balla@tup.edu.ph>
2026-08-21 14:59:46 +0800

    add the backend reference in the main runtime of the backend

--- api/index.py
+++ api/index.py
@@ -348,8 +348,13 @@ def room_admin_action(room_id):
                     else:
                         additional_updates['stickmanConfig'] = None
                 elif game_type == 'trivia-challenge':
-                    trivia_config = data.get('triviaConfig') or {}
-                    additional_updates['triviaConfig'] = trivia_config
+                    trivia_config = data.get('triviaConfig')
+                    if trivia_config:
+                        additional_updates['triviaConfig'] = trivia_config
+                    elif existing_state and existing_state.get('triviaConfig'):
+                        additional_updates['triviaConfig'] = existing_state.get('triviaConfig')
+                    else:
+                        additional_updates['triviaConfig'] = {}
             # Reset all player progress for the new game
             players_collection.update_many(
                 {'roomId': room_oid},
